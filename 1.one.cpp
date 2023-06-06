3043bf6a3ec16a9f5fd29d8664b7a10e773c59d8
Eduard Aleksandrov <eduard.ale@list.ru>
2023-06-06 17:10:12 +0300

    one init

---
+++ 1.one.cpp
@@ -0,0 +1,58 @@
+#include <iostream>               // подключаем заголовочный файл iostream
+#include <sqlite3.h>
+
+void example(void);
+
+class Person
+{
+public:
+    Person(std::string p_name) //конструктор
+    {
+        name = p_name;
+        std::cout << "Person " << name << " created" << std::endl;
+    }
+    ~Person() //Деструктор
+    {
+        std::cout << "Person " << name << " deleted" << std::endl;
+    }
+private:
+    std::string name;
+};
+
+
+int main()                          // определяем функцию main
+{                                   // начало функции
+    std::cout << "Hello World! \n";   // выводим строку на консоль
+
+// ссылка адрес и значение
+    int x {5};
+    int &y {x};
+    std::cout << y << "\n";
+    std::cout << &y << "\n";
+    std::cout << &x << "\n" << std::endl;
+// инициализация переменной
+    int z = 5.5;
+    int zz (5.5);
+    int zzz {5}; // проверяет типы, компилятор выбросит исключение при float
+    std::cout << z << "\n";
+    std::cout << zz << "\n";
+    std::cout << zzz << "\n" << std::endl;
+// bool
+    bool t;
+    t = true;
+    std::cout << t << "\n" << std::endl;
+// function prototype
+    example();
+// динамический объект
+    Person *personPtr{new Person("Tom")}; // {} можно использовать
+    delete personPtr;
+    personPtr = nullptr;
+
+
+    return 0;                       // выходим из функции
+}
+
+void example()
+{
+    std::cout << "прототип функции" << "\n";
+}
---
+++ makefile
@@ -0,0 +1,16 @@
+
+run1.one:
+	g++ -std=c++17 ./1.one.cpp -o ./build/1.one -g && ./build/1.one
+# -g подключаем gdb
+# препроцессор
+run1.one.one:
+	g++ -E ./1.one.cpp -o build/1.one.i
+# компилятор - в ассемблер
+run1.one.two:
+	g++ -std=c++17 -S ./build/1.one.i -o ./build/1.one.s
+# файл.сpp в объектный код
+run1.one.three:
+	g++ -c -std=c++17 ./1.one.cpp -o ./build/1.one.o
+# объектный код в исполняемый
+run1.one.four:
+	g++ ./build/1.one.o -o ./build/1.one && ./build/1.one
