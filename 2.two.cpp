e7bc2eb0814b29f09e11b60a337c5f62b22d6b3d
Eduard Aleksandrov <eduard.ale@list.ru>
2023-06-06 19:04:35 +0300

    отладка

--- 2.two.cpp
+++ 2.two.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "./2.two_linkfile_header.hpp"
 
+// #include <cstdio>   //нужна для std::getchar();
+
 
 
 
 int main()
 {
-    // данные из linker
+// данные из linker
     std::cout << message << times << "\n" << std::endl;
-
+    
+// отладка программы 1) std::cin.get()  2) std::exit(0)
+    // std::getchar();
+    char x = std::cin.get();   // int x - получим код цифры в ASCII
+    std::cout << x << '\n';
+    std::exit(0);
     return 0;
 }
