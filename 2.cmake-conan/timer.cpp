287b3bc99a4490b320aae0c6d4267fbd27033450
Eduard Aleksandrov <eduard.ale@list.ru>
2023-09-02 19:43:52 +0300

    conan+cmake

---
+++ 2.cmake-conan/timer.cpp
@@ -0,0 +1,35 @@
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Timer.h"
+#include "Poco/Thread.h"
+#include "Poco/Stopwatch.h"
+#include <iostream>
+
+// #include <sqlite3.h>
+
+#include <zlib.h>
+
+using Poco::Timer;
+using Poco::TimerCallback;
+using Poco::Thread;
+using Poco::Stopwatch;
+
+class TimerExample{
+public:
+        TimerExample(){ _sw.start();}
+
+        void onTimer(Timer& timer){
+                std::cout << "Callback called after " << _sw.elapsed()/1000 << " milliseconds." << std::endl;
+        }
+private:
+        Stopwatch _sw;
+};
+
+int main(int argc, char** argv){
+        TimerExample example;
+        Timer timer(250, 500);
+        timer.start(TimerCallback<TimerExample>(example, &TimerExample::onTimer));
+
+        Thread::sleep(5000);
+        timer.stop();
+        return 0;
+}
